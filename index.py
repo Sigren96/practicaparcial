ebe4778d552c1d56a3ec4b1a7d8990d48df2c0e9
Juan David <juandavidenriquez602@gmail.com>
2026-05-27 23:50:04 -0500

    separando backend y frontend

--- index.py
+++ index.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) # Esto le da permiso a Vercel para conectarse
 
 @app.route("/")
 def home():
-    # Esto busca tu HTML dentro de la carpeta 'templates'
-    return render_template("index.html")
+    # En vez de un HTML, ahora devolvemos datos puros (JSON)
+    return jsonify({
+        "estado": "Exito",
+        "mensaje": "¡El backend en Render está funcionando perfectamente!"
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
