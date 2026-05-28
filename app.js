30b4c361ef2ffc37ae9b5e126841dbd3b05298a9
Juan David <juandavidenriquez602@gmail.com>
2026-05-27 22:48:10 -0500

    inicio

---
+++ app.js
@@ -0,0 +1,19 @@
+// Atrapamos el formulario cuando el usuario le da a "Ingresar"
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault(); // Evita que la página parpadee o se recargue
+
+    // Obtenemos lo que el usuario escribió
+    const usuario = document.getElementById('usuario').value;
+    const password = document.getElementById('password').value;
+    const mensaje = document.getElementById('mensaje');
+
+    // Validación de prueba
+    if(usuario === "admin" && password === "1234") {
+        mensaje.style.color = "green";
+        mensaje.textContent = "¡Ingreso exitoso!";
+        // Aquí luego pondremos el código para redirigir a la página del modelo predictivo
+    } else {
+        mensaje.style.color = "red";
+        mensaje.textContent = "Usuario o contraseña incorrectos.";
+    }
+});
